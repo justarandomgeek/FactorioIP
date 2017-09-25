e320cf8d114c7aae450c864ba50c955c80c98f32
Danielv123 <tranen.bananen@gmail.com>
2017-09-25 20:26:00 +0200

    Added initial graphics for get-chest and made it 9x9

--- config.lua
+++ config.lua
@@ -5,8 +5,8 @@ INPUT_CHEST_ICON_PATH = nil
 
 --the chest that the player get things from
 OUTPUT_CHEST_NAME = "get-chest"
-OUTPUT_CHEST_PICTURE_PATH = nil
-OUTPUT_CHEST_ICON_PATH = nil
+OUTPUT_CHEST_PICTURE_PATH = "__clusterio__/graphics/getChest.png"
+OUTPUT_CHEST_ICON_PATH = "__base__/graphics/icons/logistic-chest-requester.png"
 
 --the tank that a player put things in
 INPUT_TANK_NAME = "put-tank"
--- data.lua
+++ data.lua
@@ -37,8 +37,8 @@ function MakeLogisticEntity(entity, name, pictureFilename, pictureTablePath, ico
 			enabled = true,
 			ingredients =
 			{
-			  {"steel-chest", 1},
-			  {"electronic-circuit", 50}
+				{"steel-chest", 1},
+				{"electronic-circuit", 50}
 			},
 			result = name,
 			requester_paste_multiplier = 4
@@ -79,11 +79,89 @@ end
 
 
 --make chests
-MakeLogisticEntity(table.deepcopy(data.raw["logistic-container"]["logistic-chest-requester"]), OUTPUT_CHEST_NAME, OUTPUT_CHEST_PICTURE_PATH, { "picture" }, OUTPUT_CHEST_ICON_PATH)
-MakeLogisticEntity(table.deepcopy(data.raw["container"]["iron-chest"]), 					    INPUT_CHEST_NAME,  INPUT_CHEST_PICTURE_PATH, { "picture" },  INPUT_CHEST_ICON_PATH)
-
+-- MakeLogisticEntity(table.deepcopy(data.raw["logistic-container"]["logistic-chest-requester"]), OUTPUT_CHEST_NAME, OUTPUT_CHEST_PICTURE_PATH, { "picture" }, OUTPUT_CHEST_ICON_PATH)
+MakeLogisticEntity(table.deepcopy(data.raw["container"]["iron-chest"]), 							INPUT_CHEST_NAME,	INPUT_CHEST_PICTURE_PATH, { "picture" },	INPUT_CHEST_ICON_PATH)
+data:extend({
+	{
+		type = "recipe",
+		name = OUTPUT_CHEST_NAME,
+		--if the recipe was succesfully attached to the tech then the recipe
+		--shouldn't be enabled to begin with.
+		--but if the recipe isn't attached to a tech then it should
+		--be enabled to begin with because otherwise the player can never use the item ingame
+		enabled = true,
+		ingredients =
+		{
+			{"steel-chest", 1},
+			{"electronic-circuit", 50}
+		},
+		result = OUTPUT_CHEST_NAME,
+		requester_paste_multiplier = 4
+	},
+	{
+		type = "item",
+		name = OUTPUT_CHEST_NAME,
+		icon = OUTPUT_CHEST_ICON_PATH,
+		flags = {"goes-to-quickbar"},
+		subgroup = "storage",
+		order = "a[items]-b["..OUTPUT_CHEST_NAME.."]",
+		place_result = OUTPUT_CHEST_NAME,
+		stack_size = 50
+	},
+	{
+		type = "logistic-container",
+		name = OUTPUT_CHEST_NAME,
+		icon = OUTPUT_CHEST_ICON_PATH,
+		flags = {"placeable-player", "player-creation"},
+		minable = {hardness = 0.2, mining_time = 2, result = OUTPUT_CHEST_NAME},
+		max_health = 2000,
+		corpse = "small-remnants",
+		collision_box = {{-7.35, -1.35}, {1.35, 7.35}},
+		selection_box = {{-7.5, -1.5}, {1.5, 7.5}},
+		resistances =
+		{
+			{
+				type = "fire",
+				percent = 90
+			},
+			{
+				type = "impact",
+				percent = 60
+			}
+		},
+		fast_replaceable_group = "container",
+		inventory_size = 48,
+		logistic_mode = "requester",
+		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
+		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
+		vehicle_impact_sound =	{ filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+		picture =
+		{
+			filename = OUTPUT_CHEST_PICTURE_PATH,
+			priority = "extra-high",
+			width = 512,
+			height = 512,
+			shift = {0.09375, 0}
+		},
+		circuit_wire_connection_point =
+		{
+			shadow =
+			{
+				red = {0.734375, 0.453125},
+				green = {0.609375, 0.515625},
+			},
+			wire =
+			{
+				red = {0.40625, 0.21875},
+				green = {0.40625, 0.375},
+			}
+		},
+		circuit_wire_max_distance = 9,
+		circuit_connector_sprites = get_circuit_connector_sprites({0.1875, 0.15625}, nil, 18),
+	},
+})
 --make tanks
-MakeLogisticEntity(table.deepcopy(data.raw["storage-tank"]["storage-tank"]),  INPUT_TANK_NAME,  INPUT_TANK_PICTURE_PATH, { "pictures", "picture", "sheet" },  INPUT_TANK_ICON_PATH)
+MakeLogisticEntity(table.deepcopy(data.raw["storage-tank"]["storage-tank"]),	INPUT_TANK_NAME,	INPUT_TANK_PICTURE_PATH, { "pictures", "picture", "sheet" },	INPUT_TANK_ICON_PATH)
 
 --------------------------------------------------------
 --[[This section is purely to create the output tank]]--
@@ -138,7 +216,7 @@ for k,v in pairs(data.raw.fluid) do
 			ingredients = {},
 			results=
 			{
-			  {type="fluid", name=v.name, amount=-1}
+				{type="fluid", name=v.name, amount=-1}
 			}
 		}
 	})
@@ -147,40 +225,40 @@ end
 
 -- Virtual signals
 data:extend{
-  {
-    type = "item-subgroup",
-    name = "virtual-signal-clusterio",
-    group = "signals",
-    order = "e"
-  },
-  {
-    type = "virtual-signal",
-    name = "signal-srctick",
-    icon = "__base__/graphics/icons/signal/signal_grey.png",
-    subgroup = "virtual-signal-clusterio",
-    order = "e[clusterio]-[1srctick]"
-  },
-  {
-    type = "virtual-signal",
-    name = "signal-srcid",
-    icon = "__base__/graphics/icons/signal/signal_grey.png",
-    subgroup = "virtual-signal-clusterio",
-    order = "e[clusterio]-[2srcid]"
-  },
-  {
-    type = "virtual-signal",
-    name = "signal-localid",
-    icon = "__base__/graphics/icons/signal/signal_grey.png",
-    subgroup = "virtual-signal-clusterio",
-    order = "e[clusterio]-[3localid]"
-  },
-  {
-    type = "virtual-signal",
-    name = "signal-unixtime",
-    icon = "__base__/graphics/icons/signal/signal_grey.png",
-    subgroup = "virtual-signal-clusterio",
-    order = "e[clusterio]-[4unixtime]"
-  },
+	{
+		type = "item-subgroup",
+		name = "virtual-signal-clusterio",
+		group = "signals",
+		order = "e"
+	},
+	{
+		type = "virtual-signal",
+		name = "signal-srctick",
+		icon = "__base__/graphics/icons/signal/signal_grey.png",
+		subgroup = "virtual-signal-clusterio",
+		order = "e[clusterio]-[1srctick]"
+	},
+	{
+		type = "virtual-signal",
+		name = "signal-srcid",
+		icon = "__base__/graphics/icons/signal/signal_grey.png",
+		subgroup = "virtual-signal-clusterio",
+		order = "e[clusterio]-[2srcid]"
+	},
+	{
+		type = "virtual-signal",
+		name = "signal-localid",
+		icon = "__base__/graphics/icons/signal/signal_grey.png",
+		subgroup = "virtual-signal-clusterio",
+		order = "e[clusterio]-[3localid]"
+	},
+	{
+		type = "virtual-signal",
+		name = "signal-unixtime",
+		icon = "__base__/graphics/icons/signal/signal_grey.png",
+		subgroup = "virtual-signal-clusterio",
+		order = "e[clusterio]-[4unixtime]"
+	},
 }
 
 -- TX Combinator
@@ -188,29 +266,29 @@ local tx = table.deepcopy(data.raw["decider-combinator"]["decider-combinator"])
 tx.name = TX_COMBINATOR_NAME
 tx.minable.result = TX_COMBINATOR_NAME
 data:extend{
-  tx,
-  {
-    type = "item",
-    name = TX_COMBINATOR_NAME,
-    icon = tx.icon,
-    flags = {"goes-to-quickbar"},
-    subgroup = "storage",
-    place_result=TX_COMBINATOR_NAME,
-    order = "a[items]-b["..TX_COMBINATOR_NAME.."]",
-    stack_size = 50,
-  },
-  {
-    type = "recipe",
-    name = TX_COMBINATOR_NAME,
-    enabled = true, -- TODO do this on a tech somewhere
-    ingredients =
-    {
-      {"decider-combinator", 1},
-      {"electronic-circuit", 50}
-    },
-    result = TX_COMBINATOR_NAME,
-    requester_paste_multiplier = 1
-  },
+	tx,
+	{
+		type = "item",
+		name = TX_COMBINATOR_NAME,
+		icon = tx.icon,
+		flags = {"goes-to-quickbar"},
+		subgroup = "storage",
+		place_result=TX_COMBINATOR_NAME,
+		order = "a[items]-b["..TX_COMBINATOR_NAME.."]",
+		stack_size = 50,
+	},
+	{
+		type = "recipe",
+		name = TX_COMBINATOR_NAME,
+		enabled = true, -- TODO do this on a tech somewhere
+		ingredients =
+		{
+			{"decider-combinator", 1},
+			{"electronic-circuit", 50}
+		},
+		result = TX_COMBINATOR_NAME,
+		requester_paste_multiplier = 1
+	},
 }
 
 -- RX Combinator
@@ -219,30 +297,30 @@ rx.name = RX_COMBINATOR_NAME
 rx.minable.result = RX_COMBINATOR_NAME
 rx.item_slot_count = 100
 data:extend{
-  rx,
-  {
-    type = "item",
-    name = RX_COMBINATOR_NAME,
-    icon = rx.icon,
-    flags = {"goes-to-quickbar"},
-    subgroup = "storage",
-    place_result=RX_COMBINATOR_NAME,
-    order = "a[items]-b["..RX_COMBINATOR_NAME.."]",
-    stack_size = 50,
-  },
-  {
-    type = "recipe",
-    name = RX_COMBINATOR_NAME,
-    enabled = true, -- TODO do this on a tech somewhere
-    ingredients =
-    {
-      {"constant-combinator", 1},
-      {"electronic-circuit", 3},
-      {"advanced-circuit", 1}
-    },
-    result = RX_COMBINATOR_NAME,
-    requester_paste_multiplier = 1
-  },
+	rx,
+	{
+		type = "item",
+		name = RX_COMBINATOR_NAME,
+		icon = rx.icon,
+		flags = {"goes-to-quickbar"},
+		subgroup = "storage",
+		place_result=RX_COMBINATOR_NAME,
+		order = "a[items]-b["..RX_COMBINATOR_NAME.."]",
+		stack_size = 50,
+	},
+	{
+		type = "recipe",
+		name = RX_COMBINATOR_NAME,
+		enabled = true, -- TODO do this on a tech somewhere
+		ingredients =
+		{
+			{"constant-combinator", 1},
+			{"electronic-circuit", 3},
+			{"advanced-circuit", 1}
+		},
+		result = RX_COMBINATOR_NAME,
+		requester_paste_multiplier = 1
+	},
 }
 -- Inventory Combinator
 local inv = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
@@ -250,27 +328,27 @@ inv.name = INV_COMBINATOR_NAME
 inv.minable.result = INV_COMBINATOR_NAME
 inv.item_slot_count = 500
 data:extend{
-  inv,
-  {
-    type = "item",
-    name = INV_COMBINATOR_NAME,
-    icon = inv.icon,
-    flags = {"goes-to-quickbar"},
-    subgroup = "storage",
-    place_result=INV_COMBINATOR_NAME,
-    order = "a[items]-b["..INV_COMBINATOR_NAME.."]",
-    stack_size = 50,
-  },
-  {
-    type = "recipe",
-    name = INV_COMBINATOR_NAME,
-    enabled = true, -- TODO do this on a tech somewhere
-    ingredients =
-    {
-      {"constant-combinator", 1},
-      {"electronic-circuit", 50}
-    },
-    result = INV_COMBINATOR_NAME,
-    requester_paste_multiplier = 1
-  },
+	inv,
+	{
+		type = "item",
+		name = INV_COMBINATOR_NAME,
+		icon = inv.icon,
+		flags = {"goes-to-quickbar"},
+		subgroup = "storage",
+		place_result=INV_COMBINATOR_NAME,
+		order = "a[items]-b["..INV_COMBINATOR_NAME.."]",
+		stack_size = 50,
+	},
+	{
+		type = "recipe",
+		name = INV_COMBINATOR_NAME,
+		enabled = true, -- TODO do this on a tech somewhere
+		ingredients =
+		{
+			{"constant-combinator", 1},
+			{"electronic-circuit", 50}
+		},
+		result = INV_COMBINATOR_NAME,
+		requester_paste_multiplier = 1
+	},
 }
