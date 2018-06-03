fd73815be8d16ddf280d03caaecaa01493e07576
Daniel Vestøl <tranen.bananen@gmail.com>
2018-06-03 11:22:13 +0200

    Attempted to fix 3rd party fluids

--- data.lua
+++ data.lua
@@ -246,28 +246,6 @@ fluidCreator.energy_usage = "1kW"
 fluidCreator.ingredient_count = 1
 fluidCreator.module_specification.module_slots = 0
 
-for k,v in pairs(data.raw.fluid) do
-	data:extend(
-	{
-		{
-			type = "recipe",
-			name = ("get-"..v.name),
-			icon = v.icon,
-			icon_size = 32,
-			category = CRAFING_FLUID_CATEGORY_NAME,
-			--localised_name = {v.name},
-			energy_required = 1,
-			subgroup = "fill-barrel",
-			order = "b[fill-crude-oil-barrel]",
-			enabled = true,
-			ingredients = {},
-			results=
-			{
-				{type="fluid", name=v.name, amount=-1}
-			}
-		}
-	})
-end
 
 --------------------------------------
 --[[Making electric tranfer things]]--
